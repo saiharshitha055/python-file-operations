464aed237802d1f5cd92bf5b8c31441973d70d7a
KOLLURI VENKATA SAI HARSHITHA <bujjiluharshikolluri@gmail.com>
2025-07-23 23:43:45 +0530

    Add files via upload

---
+++ mexe.py
@@ -0,0 +1,6 @@
+'''try:
+    num=int(input("enter a number:"))
+    print(num**3)
+except(KeyboardInterrupt, ValueError, TypeError):
+    print("please check before executing....")
+print("program terminated....") '''
